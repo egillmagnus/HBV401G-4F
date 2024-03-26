21a0bb7deafc1f60850763e1aa9ddb22a9a8acf4
solvisantos22 <124153607+solvisantos22@users.noreply.github.com>
2024-03-26 01:29:07 +0000

    Update Passenger.java

--- flightbooking/src/main/java/com/flight/domain/Passenger.java
+++ flightbooking/src/main/java/com/flight/domain/Passenger.java
@@ -1,29 +1,59 @@
 package com.flight.domain;
 
+/**
+ * Represents a passenger in the flight reservation system.
+ * A passenger is identified by their name and social security number.
+ */
 public class Passenger {
     private String name;
     private String socialSecurityNo;
 
+    /**
+     * Constructs a new Passenger with the specified name and social security number.
+     *
+     * @param name The name of the passenger.
+     * @param socialSecurityNo The social security number of the passenger.
+     */
     public Passenger(String name, String socialSecurityNo) {
         this.name = name;
         this.socialSecurityNo = socialSecurityNo;
     }
 
+    /**
+     * Returns the name of the passenger.
+     *
+     * @return The name of the passenger.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the social security number of the passenger.
+     *
+     * @return The social security number of the passenger.
+     */
     public String getSocialSecurityNo() {
         return socialSecurityNo;
     }
 
     // Setters
+    
+    /**
+     * Sets the name of the passenger.
+     *
+     * @param name The new name of the passenger.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets the social security number of the passenger.
+     *
+     * @param socialSecurityNo The new social security number of the passenger.
+     */
     public void setSocialSecurityNo(String socialSecurityNo) {
         this.socialSecurityNo = socialSecurityNo;
     }
-
 }
